864fada79b82e436f4371d067b012a2ea7302e3b
Hank Ryan Thompson <ryan.hank@gmail.com>
2013-12-07 21:30:23 -0800

    New header. New card-based group layout.

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,12 +1,18 @@
 module GroupsHelper
 
-  def group_image_tag_for(group, options = { :size => 'large' })
-    size = 200;
-
+  def group_image_tag_for(group)
     if group.image_url && !group.image_url.empty?
-      image_tag(group.image_url, size: size, class: "group-image group-image-#{options[:size]}", alt: "Learning Group Image")
+      image_tag(group.image_url, size: size, class: "group-image", alt: "Learning Group Image")
     else
-      image_tag('group-image-default.png', size: size, class: "group-image group-image-#{options[:size]}", alt: "Learning Group Image")
+      image_tag('group-image-default.png', size: size, class: "group-image", alt: "Learning Group Image")
+    end
+  end
+
+  def group_image_url_for(group)
+    if group.image_url && !group.image_url.empty?
+      group.image_url
+    else
+      image_path('group-image-default.png')
     end
   end
 
