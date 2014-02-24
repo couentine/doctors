c019bf654125618e2e372b12922395ee43b7e468
Hank Ryan Thompson <ryan.hank@gmail.com>
2014-02-23 23:05:32 -0800

    Added basic image preview style youtube embeds.

--- lib/modules/string_tools.rb
+++ lib/modules/string_tools.rb
@@ -6,6 +6,13 @@ module StringTools
   IMG_REGEX = /(?:png|jpe?g|gif|svg)$|^img:/i
   HASHTAG_REGEX = /#[\w-]+/
   SECTION_DIVIDER_REGEX = /-+\s*<br *\/?>\s*/i
+  
+  LINKS_TO_TRANSLATE = [
+    [/youtube.com.*(?:\/|v=)([^&$]+)/,
+      "<a href='https://www.youtube.com/watch?v={id}' class='linkified-video youtube' target='_blank'>
+        <img src='http://img.youtube.com/vi/{id}/0.jpg'>
+      </a>"]
+  ]
 
   # Replaces links and hashtags with valid anchor tags
   # Also replace links that end with image extensions into image tags
@@ -35,12 +42,31 @@ module StringTools
   end
 
   def make_tag_for(url)
-    if url[IMG_REGEX]
-      stripped_url = url.sub(/^img:/i, '')
-      "<a class='linkified-image' href='#{stripped_url}' target='_blank'><img src='#{stripped_url}'/></a>"
-    else
-      "<a class='linkified-url' href='#{url}' target='_blank'>#{url}</a>"
+    found_one = false
+    the_id = nil
+    return_text = nil
+
+    LINKS_TO_TRANSLATE.each do |link_pair|
+      if the_id.nil?
+        if url[link_pair.first]
+          the_id = url.match(link_pair.first)[1]
+          return_text = link_pair.last.gsub(/\{id\}/, the_id)
+        end
+      end
     end
+
+    logger.debug "+++make_tag_for: url=#{url}, the_id=#{the_id.inspect}, return_text=#{return_text}+++"
+
+    if the_id.nil?
+      if url[IMG_REGEX]
+        stripped_url = url.sub(/^img:/i, '')
+        return_text = "<a class='linkified-image' href='#{stripped_url}' target='_blank'><img src='#{stripped_url}'/></a>"
+      else
+        return_text= "<a class='linkified-url' href='#{url}' target='_blank'>#{url}</a>"
+      end
+    end
+
+    return_text
   end
 
 end
