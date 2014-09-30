f8fe6974065bf858a12a309be40110778ffe12b3
Hank Ryan Thompson <ryan.hank@gmail.com>
2014-09-30 15:07:17 -0700

    Partway through adding new contact form.

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,15 @@
+class FormsController < ApplicationController
+
+  # GET /f/talk-with-us
+  # This form allows users to schedule a time to talk with us in exchange for a free demo period
+  def user_demo
+    # Nothing to do here
+  end
+
+  # GET /f/contact-us
+  # This form allows users to schedule a time to talk with us in exchange for a free demo period
+  def contact_us
+    # Nothing to do here
+  end
+
+end
