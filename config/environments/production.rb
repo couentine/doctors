649d09e52639b02b6c1f3c363995715c847ee775
Hank Ryan Thompson <ryan.hank@gmail.com>
2015-08-03 20:12:54 -0700

    #184: Testing out cloudfront CDN in staging.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ BadgeList::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = ENV['cdn_asset_host']
+  config.action_controller.asset_host = ENV['cdn_asset_host'] || 'cdn.badgelist.com'
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += ['website.css', 'website.js', 'requirement-list-editor.css', \
