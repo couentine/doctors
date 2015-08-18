5cd41cefb560065fcdffdd5e539fd598c718990b
Hank Ryan Thompson <ryan.hank@gmail.com>
2015-08-17 21:06:43 -0700

    #186: Continuing to iterate on design and content of pricing page.

--- app/assets/javascripts/website_scripts.js
+++ app/assets/javascripts/website_scripts.js
@@ -17,4 +17,43 @@ $(document).ready(function() {
     $(this).tab('show');
   });
 
+  setupTermTooltips();
+
 });
+
+// TERM MESSAGE MAPPINGS
+
+var termMessages = {
+  "admin": "Group admins are able to create new badges, manage group membership and change "
+    + "group settings.",
+  "branding": "Branding features include custom logos in emails and the ability to define "
+    + " custom words for 'expert' and 'learner'.",
+  "community": "Community features include collaborative wiki pages and (COMING SOON) "
+    + "group profile pages and group search.",
+  "membership-controls": "Closed groups can only be joined if a user is invited by an admin.",
+  "one-badge-awarder": "The Solo plan only supports badges which are awarded by the admin.",
+  "open-groups": "Open groups allow anyone to join. All badges and evidence in open groups "
+    + "are public and indexed by search engines.",
+  "privacy-controls": "Closed groups let admins to decide between three levels of privacy for "
+    + "each badge and piece of required evidence: public, private and secret.",
+  "sub-groups": "(COMING SOON) Sub-groups allow you to link multiple groups underneath your "
+    + "primary group. This allows you to create separate collections of badges with "
+    + "a single unified membership structure.",
+  "teacher": "The Teacher plan only supports the creation and awarding of badges by a single "
+    + "teacher.",
+  "unlimited-badge-awarders": "Unlimited badge awarder plans support 3 different types of badge "
+    + "awarding: awarding by admins, awarding by badge experts and awarding by a specific "
+    + "list of users."
+};
+
+function setupTermTooltips() {
+  $(".term").each(function() {
+    var message;
+    
+    $(this).tooltip({
+      title: termMessages[$(this).data("term")],
+      placement: 'bottom',
+      container: 'body'
+    });
+  });
+}
