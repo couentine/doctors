8203ba6c3cfef1082b54758a8068bb52f77dff5c
Hank Ryan Thompson <ryan.hank@gmail.com>
2016-07-17 17:26:36 -0700

    The URL pointing at the MD5 script from Google was down so I downloaded it manually and added it to the app shared javascript assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,2 @@
 Rails.application.config.assets.precompile += ['app.js', 'embed.js', 'legacy.js', 'website.js', \
-  'app.css', 'embed.css', 'legacy.css', 'website.css', 'shared/google-code-prettify/prettify.css',
-  'shared/google-code-prettify/prettify.js']
+  'app.css', 'embed.css', 'legacy.css', 'website.css', 'shared/md5-min.js']
