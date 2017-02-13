1e4cd0a1876de33afd861e6e6f0d16ffbfa15cf9
SHEFFcode <sheff@sheffmachine.com>
2017-02-12 18:58:41 -0800

    [BL-317] bl-ajax-dialogue working for the tag view

--- app/assets/components/bl-ajax-dialog/bl-ajax-dialog.js
+++ app/assets/components/bl-ajax-dialog/bl-ajax-dialog.js
@@ -3,27 +3,27 @@
 /* ================================================== */
 
 /*
-  
-  !!!  NOTE: THIS COMPONENT IS UNFINISHED. !!!
 
-  This component displays a scrollable paper dialog filled with input items passed into it as 
-  content. It places them in a form and adds save and cancel buttons to the dialog. Clicking 
-  save will submit a query to via ajax to the specified url. Errors are presented in the form
-  success will close the dialog.
+!!!  NOTE: THIS COMPONENT IS UNFINISHED. !!!
 
-  Styling: Set the class to one of ['orange', 'green', 'blue', 'grey']
+This component displays a scrollable paper dialog filled with input items passed into it as 
+content. It places them in a form and adds save and cancel buttons to the dialog. Clicking 
+save will submit a query to via ajax to the specified url. Errors are presented in the form
+success will close the dialog.
 
-  Events:
-  - bl-ajax-dialog-cancel
-  - bl-ajax-dialog-save
-  - bl-ajax-dialog-success
-  - bl-ajax-dialog-error
+Styling: Set the class to one of ['orange', 'green', 'blue', 'grey']
+
+Events:
+- bl-ajax-dialog-cancel
+- bl-ajax-dialog-save
+- bl-ajax-dialog-success
+- bl-ajax-dialog-error
 
 */
 
 Polymer({
   is: 'bl-ajax-dialog',
-
+  
   properties: {
     title: String,
     url: {
@@ -38,34 +38,35 @@ Polymer({
     loading: Boolean
   },
   observers: [
-        // Note that this function  will not fire until *all* parameters
-        // given have been set to something other than `undefined`
-        'attributesReady(url)'
-    ],
-
+  // Note that this function  will not fire until *all* parameters
+  // given have been set to something other than `undefined`
+  'attributesReady(url)'
+  ],
+  
   attributesReady: function(url) {
-      this.$.form.action = url;
+    this.$.form.action = url;
   },
-
+  
   // Methods
   open: function() { this.$.dialog.open(); },
   close: function() { this.$.dialog.close(); },
-
+  
   // Handles the json returned by rails
-  handleSubmit: function(data) {    
-    console.log(data)
+  handleSubmit: function(data) {
+    this.fire('tagUpdate', data.detail.response.group_tag);
+    console.log(data.detail.response.group_tag);
   }, 
-
+  
   // Events
   cancelButtonTap: function(e) {
     this.close();
   },
   saveButtonTap: function(e) {
-
+    
     this.$.form.headers = JSON.parse(this.headers);
     this.$.form.submit();
-
-    this.listen(this.$.form, 'iron-form-response', this.handleSubmit())
+    
+    this.listen(this.$.form, 'iron-form-response', 'handleSubmit')
     //this.close();
   }
 });
