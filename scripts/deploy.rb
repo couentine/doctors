6ceb9f5d1abf83d940ad498ff2eb23fcd05e91b3
Hank Holiday <ryan.hank@gmail.com>
2017-07-16 13:26:39 -0700

    Issue #431: Added polymer build process to deploy script. Put test file into polymer_build_target_path which will get erased during testing.

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -1,9 +1,21 @@
+require 'fileutils'
+
 #=== CONSTANTS ===#
 
 remotes = { # first is default
   'staging' => 'bl-staging-mango', 
   'production' => 'badgelist'
 }
+
+polymer_build_options = [
+  '--js-minify',
+  '--css-minify',
+  '--html-minify',
+  '--bundle',
+  '--add-service-worker',
+  '--add-push-manifest',
+  '--insert-prefetch-links'
+]
 
 #=== SCRIPT BANNER ===#
 
@@ -12,7 +24,7 @@ remotes = { # first is default
 #=== CHECK GIT CONFIGURATION ===#
 
   puts "LOAD GIT CONFIGURATION"
-  puts "=================================================="
+  puts "================================================================================"
 
   # First we need to check that the git remotes are configured
   remotes.each do |remote_name, heroku_app_name|
@@ -41,19 +53,31 @@ remotes = { # first is default
   untracked_files = `git status --porcelain`
   if !untracked_files.empty?
     puts "===> ERROR: Uncommitted changes found...\n#{untracked_files}"
-    puts "--------------------------------------------------\n\n\n"
+    puts "--------------------------------------------------------------------------------\n\n\n"
     puts "ERROR! You have uncommitted changes in your local git environment."
     puts "Please make sure all files are committed before continuing.\n\n"
     exit
   end
 
-  puts "--------------------------------------------------\n\n\n"
+  # Get the various paths
+  root_path = `git rev-parse --show-toplevel`.chomp
+  backend_path = "#{root_path}/backend"
+  frontend_path = "#{root_path}/frontend"
+  polymer_build_source_path = "#{frontend_path}/build/default"
+  polymer_build_target_path = "#{backend_path}/public/p"
+  puts "===> Root path: #{root_path}"
+  puts "===> Backend path: #{backend_path}"
+  puts "===> Frontend path: #{frontend_path}"
+  puts "===> Polymer build source path: #{polymer_build_source_path}"
+  puts "===> Polymer build target path: #{polymer_build_target_path}"
+
+  puts "--------------------------------------------------------------------------------\n\n\n"
 
 
 #=== SELECT LOCAL BRANCH ===#
 
   puts "SELECT BRANCH TO DEPLOY"
-  puts "=================================================="
+  puts "================================================================================"
 
   puts "What local branch would you like to deploy from?"
   branches.each_with_index do |branch, index|
@@ -77,12 +101,12 @@ remotes = { # first is default
     end
   end
   
-  puts "--------------------------------------------------\n\n\n"
+  puts "--------------------------------------------------------------------------------\n\n\n"
 
 #=== SELECT DEPLOYMENT TARGET ===#
 
   puts "SELECT DEPLOYMENT TARGET"
-  puts "=================================================="
+  puts "================================================================================"
 
   puts "Where would you like to deploy to?"
   remotes.each do |remote_name, heroku_app_name|
@@ -102,15 +126,12 @@ remotes = { # first is default
     end
   end
 
-  puts "--------------------------------------------------\n\n\n"
+  puts "--------------------------------------------------------------------------------\n\n\n"
 
 #=== DO THE DEPLOYMENT ===#
 
-  git_command = "git push #{selected_remote} " \
-    + "`git subtree split --prefix backend #{selected_branch}`:master --force"
-
   puts "EXECUTE DEPLOYMENT"
-  puts "=================================================="
+  puts "================================================================================"
 
   puts "===> Selected Branch: #{selected_branch}"
   puts "===> Selected Target: #{selected_remote}"
@@ -118,7 +139,7 @@ remotes = { # first is default
   # Ensure that production deployments can only happen from master branch
   if (selected_remote == 'production') && (selected_branch != 'master')
     puts "===> ERROR: Production deployments must be from master branch"
-    puts "--------------------------------------------------\n\n\n"
+    puts "--------------------------------------------------------------------------------\n\n\n"
     puts "ERROR! Production deployments are only allowed from the master branch. "\
       + "Here are the steps..."
     puts "1) Sync all of your changes up to your Github feature branch"
@@ -128,13 +149,38 @@ remotes = { # first is default
     puts "5) Run this script again\n\n"
     exit
   end
+  
+  # Switch to the selected git branch, do the polymer build, commit the changes
+  puts "--------------------------------------------------------------------------------"
+  puts "===> REBUILDING POLYMER & COMMITTING TO GIT..."
+  system("git checkout #{selected_branch}")
+  Dir.chdir(frontend_path) do
+    system("polymer build #{polymer_build_options.join(' ')}")
+  end
+  FileUtils.remove_dir(polymer_build_target_path) # clear existing build
+  FileUtils.move(polymer_build_source_path, polymer_build_target_path) # move built files
+  system("git add .")
+  system("git commit -m 'Rebuilt polymer frontend.'")
 
+  puts "#===[TEMP EXIT... SUCCESSFUL!]===#\n\n"
+  exit
+  
+  # Push the changes to github
+  puts "--------------------------------------------------------------------------------"
+  puts "===> PUSHING POLYMER REBUILD TO REMOTE ORIGIN..."
+  system("git push origin #{selected_branch}")
+
+  # Deploy to heroku
+  puts "--------------------------------------------------------------------------------"
   puts "===> PUSHING BACKEND SUBTREE TO HEROKU..."
-  puts "===> GIT COMMAND ===> #{git_command}"
+  heroku_deploy_command = "git push #{selected_remote} " \
+    + "`git subtree split --prefix backend #{selected_branch}`:master --force"
+  puts "===> GIT COMMAND ===> #{heroku_deploy_command}"
 
-  system(git_command)
+  system(heroku_deploy_command)
+  system("git checkout #{current_branch}") # restore original branch
 
-  puts "--------------------------------------------------\n\n\n"
+  puts "--------------------------------------------------------------------------------\n\n\n"
 
 
 #=== SCRIPT FOOTER ===#
