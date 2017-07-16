1dd040aaa8b8c72fa3c10ea6e156aba510039f40
Hank Holiday <ryan.hank@gmail.com>
2017-07-16 14:14:29 -0700

    Rebuilt polymer frontend.

--- backend/public/p/service-worker.js
+++ backend/public/p/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","e43394994ee9d4d54303194dd8774ea5"]];
+var precacheConfig = [["index.html","30be4f404b05024dcc565ea99aeda6c5"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
