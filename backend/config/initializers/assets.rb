ecf79cffe9bc352c015e1ff4bc6ac91bb5dfc7e2
Hank Holiday <ryan.hank@gmail.com>
2017-07-25 21:38:21 -0700

    Issue #431: Got the basic app container working for the home view. - Purchased a new icon set to replace font awesome (it wasn't working and I wanted to upgrade the icon style anyway) - Added new icon set to rails asset pipeline (the font itself needs to be served from the root page) - Removed font awesome from polymer frontend - Finished building out the functionality of the bl-header-panel - Finished building the shared styles in bl-styles (typography, shadows, colors, etc) - Made tweaks to the app component mixin - Changed the way the app navs work

--- backend/config/initializers/assets.rb
+++ backend/config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += ['app.js', 'embed.js', 'legacy.js', 'website.js', \
-  'web.js', 'app.css', 'embed.css', 'legacy.css', 'website.css', 'web.css', 'shared/md5-min.js']
+  'web.js', 'app.css', 'embed.css', 'legacy.css', 'website.css', 'web.css', 'shared/md5-min.js', \
+  'shared/bl-icons/style.css', 'shared/bl-icons/fonts/*.*']
