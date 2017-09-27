47f0daeee193f65bddc2aadd0d071802f501489f
Hank Holiday <ryan.hank@gmail.com>
2017-09-26 17:28:01 -0700

    Issue #467: Updated Google SSO. - Found a bug when the user account isn't persisted that tried to store more info in the session cookie than could be stored there. Removed that line altogether. - Added more error-catching code to Google SSO. - Errors are now captured and stored in info items. - Errors should also result in a more clear message to the user.

--- backend/app/controllers/users/omniauth_callbacks_controller.rb
+++ backend/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,22 +1,44 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    is_error = false
     
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication # throws if @user is not activated
-      if is_navigational_format?
-        set_flash_message(:notice, :success, :kind => "Google") 
+    begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication # throws if @user is not activated
+        if is_navigational_format?
+          set_flash_message(:notice, :success, :kind => "Google") 
+        end
+      else
+        InfoItem.new(
+          type: 'google-oauth2-error', 
+          name: 'Google SSO Problem (User Not Persisted)',
+          data: { user_record: @user.inspect, errors: @user.errors.to_h, request_details: request.env['omniauth.auth'] }
+        ).save
+        is_error = true
       end
-    else 
+    rescue Exception => e
+      InfoItem.new(
+        type: 'google-oauth2-error', 
+        name: 'Google SSO Problem (Error Thrown)',
+        data: { error: e.to_s, request_details: request.env['omniauth.auth'] }
+      ).save
+      is_error = true
+    end
+
+    if is_error
       # There was some sort of problem saving (maybe a missing email) so redirect to signup instead
-      session["devise.google_oauth2_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      flash[:error] = 'There was a problem creating an account for you using Google sign in. ' \
+        + 'Please try creating an account and password manually.'
+      redirect_to new_user_registration_url(sso_error: true)
     end
   end
 
   def failure
-    redirect_to root_path
+    flash[:error] = 'There was a problem using single-sign on. Please try signing in normally or creating an account and password manually.'
+    redirect_to new_user_registration_url(sso_error: true)
   end
 
 end
