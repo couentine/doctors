8f1577d281c9210d4bc88429c5214282d4c82d88
Hank Holiday <ryan.hank@gmail.com>
2018-01-26 10:07:00 -0800

    Issue #475: - Created a rails partial to hold the service worker load script, then added it to all remaining rails layouts. - Went through and added more service worker route registrations for the various sources. - Decided against enabling offline mode (by caching html pages in the app routes) because it would result in a poor ux without more though / work

--- dev/bl-service-worker/src/sw.js
+++ dev/bl-service-worker/src/sw.js
@@ -11,18 +11,21 @@ importScripts('workbox-sw.dev.v2.1.2.js');
 const workboxSW = new WorkboxSW();
 workboxSW.precache([]);
 
-workboxSW.router.registerRoute(/assets\/.*\.(?:png|gif|jpg|ttf|css|js)/,
+// Dev Assets
+workboxSW.router.registerRoute(/http:\/\/localhost:4000\/assets\/.*\.(?:png|gif|jpg|ttf|css|js)/,
   workboxSW.strategies.cacheFirst({
     cacheName: 'assets-cache',
     cacheExpiration: {
-      maxEntries: 100
+      maxEntries: 100,
+      maxAgeSeconds: 604800 /* 1 week */
     },
     cacheableResponse: {statuses: [0, 200]}
   })
 );
 
-workboxSW.router.registerRoute(/p\/.*/,
-  workboxSW.strategies.cacheFirst({
+// Polymer Build Files
+workboxSW.router.registerRoute(/\/p\/.*/,
+  workboxSW.strategies.staleWhileRevalidate({
     cacheName: 'polymer-cache',
     cacheExpiration: {
       maxEntries: 500
@@ -30,11 +33,36 @@ workboxSW.router.registerRoute(/p\/.*/,
   })
 );
 
+// S3
+workboxSW.router.registerRoute(/https:\/\/[\w-]+\.s3.amazonaws.com\/.*/,
+  workboxSW.strategies.cacheFirst({
+    cacheName: 's3-cache',
+    cacheExpiration: {
+      maxEntries: 500,
+      maxAgeSeconds: 604800 /* 1 week */
+    },
+    cacheableResponse: {statuses: [0, 200]}
+  })
+);
+
+// Google Fonts
 workboxSW.router.registerRoute(/https:\/\/(fonts\.googleapis\.com|fonts\.gstatic\.com)\/.*/,
   workboxSW.strategies.cacheFirst({
     cacheName: 'fonts-cache',
     cacheExpiration: {
-      maxEntries: 5
+      maxEntries: 5,
+      maxAgeSeconds: 604800 /* 1 week */
+    }
+  })
+);
+
+// Intercom
+workboxSW.router.registerRoute(/https:\/\/(js\.intercomcdn\.com|static\.intercomassets\.com)\/.*/,
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'intercom-cache',
+    cacheExpiration: {
+      maxEntries: 50,
+      maxAgeSeconds: 604800 /* 1 week */
     }
   })
 );
