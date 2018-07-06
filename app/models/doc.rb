b977310eb1bdf7a0790c12d2d9aac640d1a361fc
Quentin <quent.guillaume@gmail.com>
2018-07-06 11:03:26 -0700

    search bar update

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,5 +1,5 @@
 class Doc < ApplicationRecord
   def self.search(search)
-    where("name LIKE ? OR specialty LIKE ? OR zip = ? OR review = ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    where("name LIKE ? OR specialty LIKE ?", "%#{search}%", "%#{search}%")
   end
 end
