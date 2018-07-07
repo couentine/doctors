55e22f8d7b7892f8177cf5e44c2000ea9a32a3cb
Quentin <quent.guillaume@gmail.com>
2018-07-06 17:24:41 -0700

    case sensitive

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,5 +1,5 @@
 class Doc < ApplicationRecord
   def self.search(search)
-    where("name LIKE ? OR specialty LIKE ?", "%#{search}%", "%#{search}%")
+    where("name LIKE ? OR specialty LIKE ?", "%#{search.downcase}%", "%#{search.downcase}%")
   end
 end
